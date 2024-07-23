5d5a09de97b9e7cae7f3cb8ae28d50668f47965e
yash <yash.kushwaha@invest4edu.com>
2024-07-23 15:32:15 +0530

    logs using master cluster in nodejs

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
+import cluster from "cluster";
+import os from "os";
 import express from "express";
 import router from "./routes/writeLog.js";
 
-const app = express();
+const numCPUs = os.cpus().length;
 
-app.use(express.json());
+if (cluster.isMaster) {
+    console.log(`Master process ${process.pid} is running`);
+  
+    for (let i = 0; i < numCPUs; i++) {
+      cluster.fork();
+    }
+  
+    cluster.on('exit', (worker, code, signal) => {
+      console.log(`Worker process ${worker.process.pid} died. Restarting...`);
+      cluster.fork();
+    });
+  } else {
+      const app = express();
+      
+      app.use(express.json());
+      
+      const writeLogController = router
+      
+      app.use("/api/v1/", writeLogController)
+      
+      const server = app.listen(3000, () => console.log("server running on port 3000"));
+  }
 
-const writeLogController = router
+  // Why to use master cluster refer below link for refrence 
 
-app.use("/api/v1/", writeLogController)
-
-app.listen(3000, () => console.log("server running on port 3000"));
+  //https://medium.com/@mjdrehman/implementing-node-js-cluster-for-improved-performance-f800146e58e1
